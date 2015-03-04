c85f1254b4986b4dfec488c14dddd729b349dea4
Gilmar Pupo <g@g1mr.com>
2015-03-04 10:37:16 -0300

    tests

--- tests/Gpupo/Tests/TestCaseAbstract.php
+++ tests/Gpupo/Tests/TestCaseAbstract.php
@@ -4,4 +4,12 @@ namespace Gpupo\Tests;
 
 abstract class TestCaseAbstract extends \PHPUnit_Framework_TestCase
 {
+    protected function getConstant($name, $default = false)
+    {
+        if (defined($name)) {
+            return constant($name);
+        }
+
+        return $default;
+    }
 }
